d4c33df5d9fcb3f5dbb6a579e47b303b96322c1a
Sone <sthaya@gmail.com>
2020-08-02 18:59:19 -0700

    started tik tok clone project, deleted unnecessary files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD58K-w18xwUY4wxzl9o6SAFQXwMJpRT8M",
+  authDomain: "tik-tok-clone-b36ae.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-b36ae.firebaseio.com",
+  projectId: "tik-tok-clone-b36ae",
+  storageBucket: "tik-tok-clone-b36ae.appspot.com",
+  messagingSenderId: "16887184816",
+  appId: "1:16887184816:web:ba0e67737fd6e2cfdaea75",
+  measurementId: "G-TFBF8GXS2T"
+};
